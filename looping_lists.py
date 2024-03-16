f50d3d69f087b69fdd0244d83843dc0996c12fe0
BadassMcBoss <jzepeda031898@gmail.com>
2024-03-16 10:54:07 -0500

    updating assignment

--- looping_lists.py
+++ looping_lists.py
@@ -7,17 +7,28 @@ count = 1
 
 for track in genres:
     print(f"Now playing {count}. {track}. Great choice!")
-    count += 1
-    
-    
+    count += 1  
 
 
 # Task 2: The Remix Artist with while
 # Convert the for loop from Task 1 into a while loop. 
 # Ensure it performs the same function but also includes a condition to stop the loop if a certain genre is played for instance Hip-hop.
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+count = 1
 
+while count < len(genres):
+    genre = genres[count - 1]
+    print(f"Now playing {count}. {genre}. Great choice!")
+    if genre == 'Hip-hop':
+        break
+    
+    count += 1  
 
 # Task 3: Light Show Technician Loop
 # Using the range() function, loop through the genres list by index.
 # For each genre, print out the track number and a message that the light show is ready.
 # Modify the loop to skip a genre if it's not suitable for the light show, for instance Classical genre.
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+
+for genre in range(len(genres)):
+    print(f"Track {genres + 1}: Light show is ready for {genre}!")
